5a54077dcc24a3650d01d17f8c3d00737cd4aa27
Anjali Pandey <116626593+Anjali0105-pandey@users.noreply.github.com>
2023-09-04 22:57:22 +0530

    Update max_element.java

--- max_element.java
+++ max_element.java
@@ -1,3 +1,15 @@
+/*BRUTE FORCE
+//Time Complexity : O(nlogn) For Sorting
+public class max_element {
+    public static void main(String[] args) {
+        int arr[]={10,7,-5,8,9,0,-4};
+        Arrays.sort(arr);
+        System.out.println(arr[n-1]);
+    }
+}
+*/
+//OPTIMISED
+//Time Complexity : O(N)
 public class max_element {
     public static void main(String[] args) {
         int arr[]={10,7,-5,8,9,0,-4};
